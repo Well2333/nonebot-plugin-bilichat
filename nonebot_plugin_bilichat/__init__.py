a7819405aab9a8c8232f3b0498ea4e05a9ada58d
Well404 <40534114+Well2333@users.noreply.github.com>
2023-06-05 00:38:24 +0800

    为 plugin meta 新增必填字段 (#43)

--- nonebot_plugin_bilichat/__init__.py
+++ nonebot_plugin_bilichat/__init__.py
@@ -59,6 +59,7 @@ __plugin_meta__ = PluginMetadata(
     description="一个通过 OpenAI 来对b站视频进行总结插件",
     usage="直接发送视频链接即可",
     homepage="https://github.com/Aunly/nonebot-plugin-bilichat",
+    type="application",
     supported_adapters={"~onebot.v11", "~onebot.v12", "~qqguild", "~mirai2"},
     extra={
         "author": "djkcyl & Well404",
