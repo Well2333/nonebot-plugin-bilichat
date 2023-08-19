5704effcfbdb3d9100984e0159fc03239039500f
Well404 <40534114+Well2333@users.noreply.github.com>
2023-08-19 16:08:51 +0800

    :zap: 将 dynamicrender 替换为 skia 版本 (#90)
    * :bug: :pencil: 修复 dynamic api 导入点，移除print，修改补充文档
    
    * :zap: 将 dynamicrender 替换为 skia 版本

--- nonebot_plugin_bilichat/summary/text_to_image.py
+++ nonebot_plugin_bilichat/summary/text_to_image.py
@@ -1,51 +1,54 @@
-from io import BytesIO
 import re
+from io import BytesIO
 from pathlib import Path
 
+import skia
 from dynamicadaptor.Content import RichTextDetail, Text
-from minidynamicrender.DynConfig import ConfigInit
-from minidynamicrender.DynText import DynTextRender
+from dynrender_skia.DynConfig import SetDynStyle
+from dynrender_skia.DynText import BiliText
 from nonebot.log import logger
 
 from ..config import plugin_config
-from ..lib.fonts_provider import get_font
+from ..lib.fonts_provider import get_font_sync
 from ..lib.store import cache_dir
 
+data_path = cache_dir.joinpath("render")
+data_path.mkdir(parents=True, exist_ok=True)
+browser_cookies_file = Path(plugin_config.bilichat_bilibili_cookie or "")
+
+
+render = BiliText(
+    static_path=str(data_path),
+    style=SetDynStyle(
+        font_family=str(get_font_sync("HarmonyOS_Sans_SC_Medium.ttf")),
+        emoji_font_family=str(get_font_sync("nte.ttf")),
+        font_style="Normal",
+    ).set_style,
+)
 cache = cache_dir.joinpath("text2image")
 cache.mkdir(0o755, parents=True, exist_ok=True)
 cache = str(cache.absolute())
 
 
 async def rich_text2image(data: str, src: str):
-    config = ConfigInit(
-        data_path=cache,
-        font_path={
-            "text": str(await get_font("HarmonyOS_Sans_SC_Medium.ttf")),
-            "extra_text": str(await get_font("sarasa-mono-sc-bold.ttf")),
-            "emoji": str(await get_font("nte.ttf")),
-        },
-    )
-    data = f"AI Summarization, Powered by A60 & Well404 \nNLP Model: {src}\n==========================================={data}"
-    if config.dyn_color and config.dyn_font and config.dy_size:
-        render = DynTextRender(config.static_path, config.dyn_color, config.dyn_font, config.dy_size)
-        image = await render.run(
-            Text(
-                text=data,
-                topic=None,
-                rich_text_nodes=[
-                    RichTextDetail(type="RICH_TEXT_NODE_TYPE_TEXT", text=data, orig_text=data, emoji=None)
-                ],
-            )
+    data = f"AI Summarization, Powered by A60 & Well404 \nNLP Model: {src}\n====================================\n{data}"
+    image_array = await render.run(
+        Text(
+            text=data,
+            topic=None,
+            rich_text_nodes=[RichTextDetail(type="RICH_TEXT_NODE_TYPE_TEXT", text=data, orig_text=data, emoji=None)],
         )
-        if image:
-            bio = BytesIO()
-            image.convert("RGB").save(bio, "jpeg", optimize=True)
-            return bio.getvalue()
+    )
+    bio = BytesIO()
+    img = skia.Image.fromarray(image_array, colorType=skia.ColorType.kRGBA_8888_ColorType)
+    img.save(bio)
+    return bio.getvalue()
 
 
 async def pw_text2image(data: str, src: str):
     import jinja2
     from nonebot_plugin_htmlrender import get_new_page
+
     from ..lib.browser import pw_font_injecter
 
     src = "openai" if src == plugin_config.bilichat_openai_model else "newbing"
