e77a1df776002478bb195e111792103f67efb571
Well404 <40534114+Well2333@users.noreply.github.com>
2023-12-18 00:35:49 +0800

    ✨ 添加根据 UID 订阅 UP (#155)

--- nonebot_plugin_bilichat/lib/uid_extract.py
+++ nonebot_plugin_bilichat/lib/uid_extract.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+from nonebot.log import logger
 from pydantic import BaseModel, Extra, Field
 
 from .bilibili_request import search_user
@@ -17,13 +18,21 @@ class SearchResult(BaseModel, extra=Extra.ignore):
     items: list[SearchUp] = []
 
 
-async def uid_extract(text: str) -> Union[str, SearchUp]:
-    text_u = text.strip(""""'“”‘’""").strip()
+async def search(text_u: str) -> Union[str, SearchUp]:
     resp = await search_user(text_u)
     result = SearchResult(**resp)
     if result.items:
         for up in result.items:
-            if up.nickname == text_u:
+            if up.nickname == text_u or str(up.mid) in text_u:
+                logger.debug(up)
                 return up
         return "未找到该 UP，你可能在找：\n" + "\n".join([str(up) for up in result.items])
     return "未找到该 UP 主呢\n`(*>﹏<*)′"
+
+
+async def uid_extract(text: str) -> Union[str, SearchUp]:
+    text_u = text.strip(""""'“”‘’""").strip().replace("：", ":")
+    up = await search(text_u)
+    if isinstance(up, str) and text_u.isdigit():
+        up = await search("UID: " + text_u)
+    return up
