00ef9850b88f64d582eb36b5b9e953c830e931b7
Well404 <40534114+Well2333@users.noreply.github.com>
2024-05-06 17:44:12 +0800

    :bug: fix #210 (#213)

--- nonebot_plugin_bilichat/base_content_parsing.py
+++ nonebot_plugin_bilichat/base_content_parsing.py
@@ -167,7 +167,7 @@ async def content_info(event: Event, origin_msg: UniMsg, state: T_State):
         msgs.append(reply)
         if content_image:
             msgs.append(Image(raw=content_image))
-        msgs.append(content.url if plugin_config.bilichat_basic_info_url else content.bili_id)
+        msgs.append(Text(content.url if plugin_config.bilichat_basic_info_url else content.bili_id))
         receipt = await msgs.send()
         reply = receipt.get_reply() if plugin_config.bilichat_reply_to_basic_info else reply
 
